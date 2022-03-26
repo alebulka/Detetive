2f4d952c30e8c7d7f1c2769d659fe2d009831378
alebulka <bulkalessandra@outlook.com>
2022-03-25 22:14:54 -0300

    first commit

---
+++ js/debora.js
@@ -0,0 +1,65 @@
+function escolha1() {
+    tentativa = 2;
+    while (tentativa > 0) {
+        var resposta = prompt('Qual a alternativa correta?');
+        if(resposta != null){
+            if(resposta == '2'){
+                tentativa--;
+                alert(`Você tem ${tentativa} tentativas restantes`);
+            }else if(resposta =='1'){    
+                window.location.assign('./deborafase2.html');        
+                break;
+            }else{
+                alert("Resposta inválida");
+            }
+        }
+    }
+    if (tentativa == 0 ) {
+        alert('Suas tentativas acabaram');
+        window.location.assign('../../projeto/wrong.html');
+    }
+}
+
+function escolha2() {
+    tentativa = 2;
+    while (tentativa > 0) {
+        var resposta = prompt('Qual a alternativa correta?');
+        if(resposta != null){
+            if(resposta == '2'|| resposta =='3'){
+                tentativa--;
+                alert(`Você tem ${tentativa} tentativas restantes`);
+            }else if(resposta =='1'){    
+                window.location.assign('./deborafase3.html');        
+                break;
+            }else{
+                alert("Resposta inválida");
+            }
+        }
+    }
+    if (tentativa == 0 ) {
+        alert('Suas tentativas acabaram');
+        window.location.assign('../../projeto/wrong.html');
+    }
+}
+
+function escolha3() {
+    tentativa = 2;
+    while (tentativa > 0) {
+        var resposta = prompt('Qual a alternativa correta?');
+        if(resposta != null){
+            if(resposta == '2'){
+                tentativa--;
+                alert(`Você tem ${tentativa} tentativas restantes`);
+            }else if(resposta =='1'){    
+                window.location.assign('../../projeto/victory.html');        
+                break;
+            }else{
+                alert("Resposta inválida");
+            }
+        }
+    }
+    if (tentativa == 0 ) {
+        alert('Suas tentativas acabaram');
+        window.location.assign('../../projeto/wrong.html');
+    }
+}
